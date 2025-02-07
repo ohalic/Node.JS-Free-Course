7cad890fdd4f152b15db4d9c072b0d9e9af6c6a7
6530200134Natnicha <ohalics3388@gmail.com>
2025-02-08 02:44:31 +0700

    public page

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 import express from 'express'; // เรียกใช้ express
 import chalk from 'chalk'; // เรียกใช้ chalk
-import debug from 'debug';
+import createDebug from 'debug';
+const debug = createDebug('app');
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
-const log = debug('app'); // สร้างตัวแปร log สำหรับ debug
 const app = express(); // สร้างตัวแปร app เรียกใช้ express
 const port = 3000; // กำหนด port 3000
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(_dirname, "/public/")));
 
 app.get("/", (req, res) => {
-    res.send('Hello bornToDev UIIAIUIIIAI MEOW'); // ส่งข้อความกลับไป
+    res.send('Hello bornToDev UIIAIUIIIAI MEOW YuMMMMy'); // ส่งข้อความกลับไป
 });
 
 app.listen(port, () => {
